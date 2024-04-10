f900083ce749128aee49003f07f2ac2a7fe8989c
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-04-10 16:37:45 +0100

    Card component for admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,35 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function AdminDashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <DashboardCard title="sales" subtitle="Test" body="body" />
+    </div>
+  );
+}
+
+type DashboardCardProps = {
+  title: string;
+  subtitle: string;
+  body: string;
+};
+
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
+  );
 }
