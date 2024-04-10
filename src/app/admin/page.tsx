6c5e5024e4e1bc7ff75695086bbe364956faf7a1
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-04-10 17:00:44 +0100

    Creating Sales card and adding formatters

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,11 +5,30 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import db from "@/db/db";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
 
-export default function AdminDashboard() {
+async function getSalesData() {
+  const data = await db.order.aggregate({
+    _sum: { pricePaidInCents: true },
+    _count: true,
+  });
+
+  return {
+    amount: (data._sum.pricePaidInCents || 0) / 100,
+    numberOfSales: data._count,
+  };
+}
+
+export default async function AdminDashboard() {
+  const salesData = await getSalesData();
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      <DashboardCard title="sales" subtitle="Test" body="body" />
+      <DashboardCard
+        title="Sales"
+        subtitle={`${formatNumber(salesData.numberOfSales)} orders`}
+        body={formatCurrency(salesData.amount)}
+      />
     </div>
   );
 }
