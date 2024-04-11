7543213a706837029cbe7e7de28119ff870eb86c
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-04-11 23:26:31 +0100

    Customer card data setup

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -20,14 +20,42 @@ async function getSalesData() {
   };
 }
 
+async function getUserData() {
+  const [userCount, orderData] = await Promise.all([
+    db.user.count(),
+    db.order.aggregate({
+      _sum: { pricePaidInCents: true },
+    }),
+  ]);
+
+  return {
+    userCount,
+    averageValuePerUser:
+      userCount === 0
+        ? 0
+        : orderData._sum.pricePaidInCents || 0 / userCount / 100,
+  };
+}
+
 export default async function AdminDashboard() {
-  const salesData = await getSalesData();
+  const [salesData, userData] = await Promise.all([
+    getSalesData(),
+    getUserData(),
+  ]);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <DashboardCard
         title="Sales"
         subtitle={`${formatNumber(salesData.numberOfSales)} orders`}
         body={formatCurrency(salesData.amount)}
+      />
+      <DashboardCard
+        title="Customer"
+        subtitle={`${formatCurrency(
+          userData.averageValuePerUser
+        )} Average Value`}
+        body={formatNumber(userData.userCount)}
       />
     </div>
   );
