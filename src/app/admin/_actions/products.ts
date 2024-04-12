f771226484f21b0aed07a9edf3c08ac447d8baac
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-04-12 17:53:12 +0100

    Add Product form

---
+++ src/app/admin/_actions/products.ts
@@ -0,0 +1,5 @@
+"use server";
+
+export async function addProduct(formData: FormData) {
+  console.log(formData);
+}
---
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { formatCurrency } from "@/lib/formatters";
+import { useState } from "react";
+import { addProduct } from "../../_actions/products";
+
+export function ProductForm() {
+  const [priceInCents, setPriceInCents] = useState<number>();
+  return (
+    <form action={addProduct} className="space-y-8">
+      <div className="space-y-2">
+        <Label htmlFor="name">Name</Label>
+        <Input type="text" id="name" name="name" required />
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="priceInCents">Price In Cents</Label>
+        <Input
+          type="number"
+          id="priceInCents"
+          name="priceInCents"
+          required
+          value={priceInCents}
+          onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
+        />
+        <div className="text-muted-foreground">
+          {formatCurrency((priceInCents || 0) / 100)}
+        </div>
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="description">Description</Label>
+        <Textarea id="description" name="description" required />
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="file">File</Label>
+        <Input type="file" id="file" name="file" required />
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="image">Image</Label>
+        <Input type="file" id="image" name="image" required />
+      </div>
+      <Button type="submit">Save</Button>
+    </form>
+  );
+}
