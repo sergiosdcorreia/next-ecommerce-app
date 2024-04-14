d27acbd3588639f6708e316a8f0bce6dd0addafa
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-04-14 10:06:37 +0100

    Edit products page - WIP

---
+++ src/app/admin/products/[id]/edit/page.tsx
@@ -0,0 +1,11 @@
+import { PageHeader } from "../_components/PageHeader";
+import { ProductForm } from "../../_components/ProductForm";
+
+export default function EditProductPage() {
+  return (
+    <>
+      <PageHeader>Edit Product</PageHeader>
+      <ProductForm />
+    </>
+  );
+}
