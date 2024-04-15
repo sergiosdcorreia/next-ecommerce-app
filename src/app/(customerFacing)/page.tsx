95b67b0ab9075e82facd3471c50708caf6cd611b
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-04-15 23:02:27 +0100

    Frontend-Display Most Popular & Newest products

---
+++ src/app/(customerFacing)/page.tsx
@@ -0,0 +1,62 @@
+import { ProductCard } from "@/components/ProductCard";
+import { Button } from "@/components/ui/button";
+import db from "@/db/db";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
+function getMostPopularProducts() {
+  return db.product.findMany({
+    where: { isAvailableForPurchase: true },
+    orderBy: { orders: { _count: "desc" } },
+    take: 6,
+  });
+}
+
+function getNewestProducts() {
+  return db.product.findMany({
+    where: { isAvailableForPurchase: true },
+    orderBy: { createdAt: "desc" },
+    take: 6,
+  });
+}
+
+export default function HomePage() {
+  return (
+    <main className="space-y-12">
+      <ProductGridSection
+        productsFetcher={getMostPopularProducts}
+        title="Most Popular"
+      />
+      <ProductGridSection productsFetcher={getNewestProducts} title="Newest" />
+    </main>
+  );
+}
+
+type ProductGridSectionProps = {
+  title: string;
+  productsFetcher: () => Promise<Product[]>;
+};
+
+async function ProductGridSection({
+  productsFetcher,
+  title,
+}: ProductGridSectionProps) {
+  return (
+    <div className="space-y-4">
+      <div className="flex gap-4">
+        <h2 className="text-3xl font-bold">{title}</h2>
+        <Button variant="outline" asChild>
+          <Link href="/products">
+            <span>View All</span>
+            <ArrowRight className="size-4" />
+          </Link>
+        </Button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {(await productsFetcher()).map((product) => (
+          <ProductCard key={product.id} {...product} />
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,48 @@
+import { formatCurrency } from "@/lib/formatters";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import Image from "next/image";
+
+type ProductCardProps = {
+  id: string;
+  name: string;
+  priceInCents: number;
+  description: string;
+  imagePath: string;
+};
+
+export function ProductCard({
+  id,
+  name,
+  priceInCents,
+  description,
+  imagePath,
+}: ProductCardProps) {
+  return (
+    <Card className="flex overflow-hidden flex-col">
+      <div className="relative w-full h-auto aspect-video">
+        <Image src={imagePath} fill alt={name} />
+      </div>
+      <CardHeader>
+        <CardTitle>{name}</CardTitle>
+        <CardDescription>{formatCurrency(priceInCents / 100)}</CardDescription>
+      </CardHeader>
+      <CardContent className="flex-grow">
+        <p className="line-clamp-4">{description}</p>
+      </CardContent>
+      <CardFooter>
+        <Button asChild size="lg" className="w-full">
+          <Link href={`/products/${id}/purchase`}>Purchase</Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
