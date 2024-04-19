7e3d89aa661ddcd91366057303b78a7cc792db70
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-04-19 19:12:21 +0100

    Purchase Receipt email

---
+++ src/email/PurchaseReceipt.tsx
@@ -0,0 +1,53 @@
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Preview,
+  Tailwind,
+} from "@react-email/components";
+import { OrderInformation } from "./components/OrderInformation";
+
+type PurchaseReceiptEmailProps = {
+  product: {
+    name: string;
+  };
+  order: { id: string; createdAt: Date; pricePaidInCents: number };
+  downloadVerificationId: string;
+};
+
+PurchaseReceiptEmail.PreviewProps = {
+  product: { name: "Product name" },
+  order: {
+    id: crypto.randomUUID(),
+    createdAt: new Date(),
+    pricePaidInCents: 10000,
+  },
+  downloadVerificationId: crypto.randomUUID(),
+} satisfies PurchaseReceiptEmailProps;
+
+export default function PurchaseReceiptEmail({
+  product,
+  order,
+  downloadVerificationId,
+}: PurchaseReceiptEmailProps) {
+  return (
+    <Html>
+      <Preview>Download {product.name} and view receipt</Preview>
+      <Tailwind>
+        <Head />
+        <Body className="font-sans bg-white">
+          <Container className="max-w-xl">
+            <Heading>Purchase Receipt</Heading>
+            <OrderInformation
+              product={product}
+              order={order}
+              downloadVerificationId={downloadVerificationId}
+            />
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+}
---
+++ src/email/components/OrderInformation.tsx
@@ -0,0 +1,47 @@
+import { Column, Row, Section, Text } from "@react-email/components";
+import { formatCurrency } from "../../lib/formatters";
+
+type OrderInformationProps = {
+  order: { id: string; createdAt: Date; pricePaidInCents: number };
+  product: {};
+  downloadVerificationId: string;
+};
+
+const dateFormatter = new Intl.DateTimeFormat("en", { dateStyle: "medium" });
+
+export function OrderInformation({
+  order,
+  product,
+  downloadVerificationId,
+}: OrderInformationProps) {
+  return (
+    <>
+      <Section>
+        <Row>
+          <Column>
+            <Text className="mb-0 text-gray-500 whitespace-nowrap text-nowrap mr-4">
+              Order ID
+            </Text>
+            <Text className="mt-0 mr-4">{order.id}</Text>
+          </Column>
+          <Column>
+            <Text className="mb-0 text-gray-500 whitespace-nowrap text-nowrap mr-4">
+              Purchased On
+            </Text>
+            <Text className="mt-0 mr-4">
+              {dateFormatter.format(order.createdAt)}
+            </Text>
+          </Column>
+          <Column>
+            <Text className="mb-0 text-gray-500 whitespace-nowrap text-nowrap mr-4">
+              Price Paid
+            </Text>
+            <Text className="mt-0 mr-4">
+              {formatCurrency(order.pricePaidInCents / 100)}
+            </Text>
+          </Column>
+        </Row>
+      </Section>
+    </>
+  );
+}
